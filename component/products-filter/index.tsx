f3cd3cdfbea42fb618bb30e48bf23ff08a45ec89
TrungAnh <votrantrunganh@gmail.com>
2024-01-06 10:20:07 +0700

    feat: create filter choose color but not focus color show

--- component/products-filter/index.tsx
+++ component/products-filter/index.tsx
@@ -1,36 +1,77 @@
 import Link from "next/link";
 import productsTypes from "@/utils/data-products/product-types";
-import Checkbox from './form-builder/checkbox/index';
+import productsColors from "@/utils/data-products/product-color";
+import CheckboxColor from "./form-builder/checkbox-color";
+import { RadioGroup } from "@headlessui/react";
+import Checkbox from "./form-builder/checkbox/index";
 import Slider from "rc-slider";
+import { useState } from "react";
 
 // const { createSliderWithTooltip } = Slider;
 // const Range = createSliderWithTooltip(Slider.Range);
 
-const ProductFilter = () => {
-  
-
-    return (
-        <form className=" p-8 hidden lg:block sticky top-24 z-500">
-        <h3 className=" text-lg font-bold pb-4 border-b border-gray-300 py-2">
-          Các danh mục sản phẩm
-        </h3>
-        <ul
-          role="list"
-          className="space-y-4 pt-3 pb-6 text-sm font-medium text-gray-900"
-        >
-          {productsTypes.map(type => (
-            <Checkbox
-              key={type.id} 
-              label={type.name} 
-              name='Product-Type'            />
-          ))}
-        </ul>
-        <h3 className=" text-lg font-bold pb-4 border-b border-gray-300 py-2">
-          Giá
-        </h3>
-        
-      </form>
-    )
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(" ");
 }
+const handleColorChange = (selectedColor: string) => {
+  // Xử lý khi màu sắc thay đổi
+  console.log(`Selected color: ${selectedColor}`);
+};
+const ProductFilter = () => {
+  const [selectedColor, setSelectedColor] = useState(productsColors[0]);
+  return (
+    <form className=" p-8 hidden lg:block sticky top-24 z-500">
+      <h3 className=" text-lg font-bold pb-4 border-b border-gray-300 py-2">
+        Các danh mục sản phẩm
+      </h3>
+      <ul
+        role="list"
+        className="space-y-4 pt-3 pb-6 text-sm font-medium text-gray-900"
+      >
+        {productsTypes.map((type) => (
+          <Checkbox key={type.id} label={type.name} name="Product-Type" />
+        ))}
+      </ul>
 
-export default ProductFilter
+      <h3 className="text-lg font-bold pb-4 border-b border-gray-300 py-2">Chọn màu sắc</h3>
+      <RadioGroup
+        value={selectedColor}
+        onChange={setSelectedColor}
+        className="mt-4"
+      >
+        <div className="flex items-center space-x-3">
+          {productsColors.map((color) => (
+            <RadioGroup.Option
+              key={color.name}
+              value={color}
+              className={({ active, checked }) =>
+                classNames(
+                  color.selectedClass,
+                  active && checked ? "ring ring-offset-1" : "",
+                  !active && checked ? "ring-2" : "",
+                  "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 focus:outline-none"
+                )
+              }
+            >
+              <RadioGroup.Label as="span" className="sr-only">
+                {color.name}
+              </RadioGroup.Label>
+              <span
+                aria-hidden="true"
+                className={classNames(
+                  color.class,
+                  "h-8 w-8 rounded-full border border-black border-opacity-10"
+                )}
+              />
+            </RadioGroup.Option>
+          ))}
+        </div>
+      </RadioGroup>
+
+      <h3 className="text-lg font-bold pb-4 border-b border-gray-300 py-2 mt-3">Chọn khoảng giá</h3>
+
+    </form>
+  );
+};
+
+export default ProductFilter;
--- utils/data-products/product-color.ts
+++ utils/data-products/product-color.ts
@@ -1,34 +1,7 @@
 export const productsColors = [
-    {
-      id: '1',
-      label: 'Black',
-      color: '#000000',
-    },
-    {
-      id: '2',
-      label: 'Brown',
-      color: '#6F3E18',
-    },
-    {
-      id: '3',
-      label: 'Yellow',
-      color: '#D4BE8D',
-    },
-    {
-      id: '4',
-      label: 'Gray',
-      color: '#838383',
-    },
-    {
-      id: '5',
-      label: 'White',
-      color: '#F3F3F3',
-    },
-    {
-      id: '6',
-      label: 'Blue',
-      color: '#0F73AD',
-    },
-  ];
-  
-  export default productsColors;
+  { name: "White", class: "bg-white", selectedClass: "ring-red-400" },
+  { name: "Gray", class: "bg-gray-200", selectedClass: "ring-gray-400" },
+  { name: "Black", class: "bg-gray-900", selectedClass: "ring-gray-900" },
+];
+
+export default productsColors;
